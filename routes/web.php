93b95cec520e29d09cd5e3f201db556fc4c00b8e
Vishnu Gp <vishnu.aassk@gmail.com>
2021-10-17 19:31:41 +0530

    Creation and listing of patients added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', App\Http\Livewire\PatientTable::class);
+Route::get('/patient_create', App\Http\Livewire\PatientCreate::class);
