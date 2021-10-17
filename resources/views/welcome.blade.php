5067485a4a19ebf6234eb0694ac8bbc87ba21379
Vishnu Gp <vishnu.aassk@gmail.com>
2021-10-17 14:50:51 +0530

    Added Livewire and Alpine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,8 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
