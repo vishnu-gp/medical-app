64a94914f4ae1839be6e1844e0c163f1846abe46
Vishnu Gp <vishnu.aassk@gmail.com>
2021-10-18 15:37:13 +0530

    Added database seeder to main seed function

---
+++ database/migrations/2021_10_18_100038_update_patients_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patients', function (Blueprint $table) {
+            $table->dropUnique('patients_email_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
